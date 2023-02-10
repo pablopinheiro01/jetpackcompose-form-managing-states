af11dc2cbed39eda1ea63db7639750d46c11d357
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2023-02-10 10:36:33 -0300

    Criado no composable App parametros para passagem de função de click do FAB e conteudo do compose usando logica do SLOT API, extraido a logica de negocios para a camada mais superior main (RESPEITANDO PRINCIPIOS DO SOLID)!

---
+++ app/src/main/java/br/com/alura/aluvery/dao/ProductDao.kt
@@ -0,0 +1,15 @@
+package br.com.alura.aluvery.dao
+
+import br.com.alura.aluvery.sampledata.sampleProducts
+
+class ProductDao {
+
+    companion object{
+        private val products = sampleProducts.toMutableList()
+    }
+
+    fun products() = products.toList() //imutable data
+
+
+
+}
--- app/src/main/java/br/com/alura/aluvery/ui/activities/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/activities/MainActivity.kt
@@ -15,36 +15,57 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.alura.aluvery.sampledata.sampleSections
+import br.com.alura.aluvery.dao.ProductDao
+import br.com.alura.aluvery.sampledata.sampleCandies
+import br.com.alura.aluvery.sampledata.sampleDrinks
 import br.com.alura.aluvery.ui.screens.HomeScreen
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val dao = ProductDao()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App(onFabClick = {
-                startActivity(Intent(this, ProductFormActivity::class.java))
-            })
+            App(
+                onFabClick = {
+                    startActivity(
+                        Intent(
+                            this,
+                            ProductFormActivity::class.java
+                        )
+                    )
+                },
+            ) {
+                val sections = mapOf(
+                    "Todos produtos" to dao.products(),
+                    "Promocoes" to sampleDrinks + sampleCandies,
+                    "Doces" to sampleCandies,
+                    "Bebidas" to sampleDrinks
+                )
+                HomeScreen(sections = sections)
+            }
         }
     }
 }
 
 @Composable
-fun App(onFabClick: () -> Unit = {}) {
+fun App(
+    onFabClick: () -> Unit = {},
+    content: @Composable () -> Unit = {},
+) {
     AluveryTheme {
         Surface {
             Scaffold(
                 floatingActionButton = {
                     FloatingActionButton(onClick = { onFabClick() }) {
-                        Icon(imageVector = Icons.Default.Add, contentDescription = null )
+                        Icon(imageVector = Icons.Default.Add, contentDescription = null)
                     }
                 }
             ) { paddingValues ->
                 Box(modifier = Modifier.padding(paddingValues))
-                HomeScreen(
-                    sampleSections
-                )
+                content()
             }
         }
     }
