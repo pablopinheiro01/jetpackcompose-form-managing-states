2ab62a9cb813aa0c58aad14904ee6a4546ebb68f
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2023-02-10 17:51:12 -0300

    Refactory para uso do composable stateless e stateful

--- app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.alura.aluvery.model.Product
+import br.com.alura.aluvery.sampledata.sampleCandies
+import br.com.alura.aluvery.sampledata.sampleDrinks
 import br.com.alura.aluvery.sampledata.sampleProducts
 import br.com.alura.aluvery.sampledata.sampleSections
 import br.com.alura.aluvery.ui.components.CardProductItem
@@ -33,14 +35,58 @@ class HomeScreenUiState(
 
 }
 @Composable
-fun HomeScreen(){
+fun HomeScreen(products: List<Product>){ // composable statefull
 
+    val sections = mapOf(
+        "Todos produtos" to products,
+        "Promocoes" to sampleDrinks + sampleCandies,
+        "Doces" to sampleCandies,
+        "Bebidas" to sampleDrinks
+    )
+
+    var text by remember{
+        mutableStateOf("")
+    }
+
+    //logica de verificacao dos dados buscados
+    fun containsInNameOrDescription() = { product: Product ->
+        product.name.contains(
+            text,
+            ignoreCase = true,
+        ) ||
+                product.description?.contains(
+                    text,
+                    ignoreCase = true,
+                ) ?: false
+    }
+
+    val searchedProducts = remember(text,products){
+        if (text.isNotBlank()) {
+            //feito o merge dos filtros realizados nos dados
+            sampleProducts.filter(containsInNameOrDescription()) + products.filter(
+                containsInNameOrDescription()
+            )
+        } else emptyList()
+    }
+
+    //passagem via parametro para reagir a mudança realizada
+    val state = remember(sections,text) {//state vai reagir a mudanças das variaveis sections e text
+        HomeScreenUiState(
+            sections = sections,
+            searchedProducts = searchedProducts,
+            searchText = text,
+            onSearchChange = {
+                text = it
+            }
+        )
+    }
+    HomeScreen(state = state)
 }
 
 @Composable
 fun HomeScreen(
     state: HomeScreenUiState = HomeScreenUiState()
-) {
+) { //composable stateless
     Column {
         val sections = state.sections
         val text = state.searchText
