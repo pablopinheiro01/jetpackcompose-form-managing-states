d43671863bab509d4ee7264d08d8e4f3bb73153b
Pablo Pinheiro <pablo-pinheiro@hotmail.com>
2023-02-10 10:54:18 -0300

    utilizado mutablestatelistof para permitir notificacao de mudanca das listas dentro do compose

--- app/src/main/java/br/com/alura/aluvery/dao/ProductDao.kt
+++ app/src/main/java/br/com/alura/aluvery/dao/ProductDao.kt
@@ -1,15 +1,24 @@
 package br.com.alura.aluvery.dao
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.toMutableStateList
+import br.com.alura.aluvery.model.Product
 import br.com.alura.aluvery.sampledata.sampleProducts
 
 class ProductDao {
 
     companion object{
-        private val products = sampleProducts.toMutableList()
+//        private val products = sampleProducts.toMutableList() // mudamos de mutablelist para mutableStateList
+    // mutableStateList tem a capacidade de notificar a mudança dos dados
+//        private val products = mutableStateListOf<Product>(*sampleProducts.toTypedArray()) //exemplo passando sample
+        private val products = mutableStateListOf<Product>()//lista vazia
     }
 
     fun products() = products.toList() //imutable data
 
+    fun save(product: Product) {
+        products.add(product)
+    }
 
 
 }
